dcebbb2c5fc0b1a0000144a4af0964d8c06307a1
akashjaiswar14 <akash9015jaiswar@gmail.com>
2026-01-07 01:58:35 +0530

    added frontend file

---
+++ fiscally-react/src/pages/Signup.jsx
@@ -0,0 +1,141 @@
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import { assets } from '../assets/assets';
+import Input from '../components/Input';
+import { validateEmail } from '../util/validation';
+import AxiosConfig from '../util/AxiosConfig';
+import { API_ENDPOINTS } from '../util/apiEndPoints';
+import toast from 'react-hot-toast';
+import { LoaderCircle } from 'lucide-react';
+
+const Signup = () => {
+
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+    setIsLoading(true);
+    
+    // basic validation
+    if(!fullName.trim()){
+      setError("Please enter your fullname");
+      setIsLoading(false);
+      return;
+    }
+
+    if(!validateEmail(email)){
+      setError("Please enter your valid email address");
+      setIsLoading(false);
+      return;
+    }
+
+    if(!password.trim()){
+      setError("Please enter your password");
+      setIsLoading(false);
+      return;
+    }
+
+    setError("")
+
+    // signup api call
+    try {
+      const response = await AxiosConfig.post(API_ENDPOINTS.REGISTER, {
+        fullName,
+        email,
+        password
+      })
+      if(response.status === 201){
+        toast.success("Profile created successfully.")
+        navigate("/login")
+      }
+    } catch (error) {
+        console.error("something went wrong", error);
+        setError(error.message);
+    } finally{
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <div className='h-screen w-full relative flex items-center justify-center overflow-hidden'>
+      {/* {background image with blur} */}
+      <img src={assets.login_bg} alt="Background" className='absolute inset-0 w-full h-full object-cover filter blur-sm' />
+
+      <div className='relative z-10 w-full max-w-lg px-6'>
+        <div className='bg-white bg-opacity-95 backdrop-blur-sm rounded-lg shadow-2xl p-8 max-h-[90vh] overflow-y-auto'>
+          <h3 className='text-2xl font-semibold text-black text-center mb-2'>
+            Create An Account
+          </h3>
+          <p className='text-sm text-slate-700 text-center mb-8'>
+            Start tracking your spendings by joining with us.
+          </p>
+
+          <form onSubmit={handleSubmit} className='space-y-4'>
+            <div className='flex justify-center mb-6'>
+              {/* {profiel image} */}
+            </div>
+
+            <div className='grid grid-cols-2 md:grid-cols-2 gap-4'>
+              <Input 
+                value={fullName}
+                onChange={(e)=> setFullName(e.target.value)}
+                label="Full Name"
+                placeholder="Enter Full Name"
+                type="text"
+              />
+
+              <Input 
+                value={email}
+                onChange={(e)=> setEmail(e.target.value)}
+                label="Email Address"
+                placeholder="Enter Email"
+                type="email"
+              />
+
+              <div className='col-span-2'>
+                <Input 
+                  value={password}
+                  onChange={(e)=> setPassword(e.target.value)}
+                  label="Password"
+                  placeholder="Enter Password"
+                  type="password"
+                />
+              </div>
+              
+            </div>
+            {error && (
+              <p className='text-red-800 text-sm text-center bg-red-50 p-2 rounded'>
+                {error}
+              </p>
+            )}
+
+            <button disabled={isLoading} className={`btn-primary w-full py-3 text-lg font-medium flex items-center justify-center gap-2 ${isLoading ? 'opacity-60 cursor-not-allowed': ''} bg-purple-900 rounded-3xl text-white hover:bg-purple-700`} type='submit'>
+              {isLoading ? (
+                <>
+                  <LoaderCircle className='animate-spin w-5 h-5' />
+                  Signing up...
+                </>
+              ) : (
+                "Sign up"
+              )}
+            </button>
+
+            <p className='text-sm text-slate-800 text-center mt-6'>
+              Already have an account?
+              <Link to="/login" className='font-medium text-primary underline hover:text-primary-dark transition-colors'> login</Link>
+            </p>
+          </form>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Signup
